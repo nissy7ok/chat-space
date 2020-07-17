2e77923cd0380a2011359178544fd993354f7e4c
nissy7ok <nishimrua.n.ku@gmail.com>
2020-07-17 15:44:05 +0900

    fixed syntax & layouts
    * fixed syntax
      + fix space & indent miss
    * fixed layouts
      + add css of 'field_with_errors' to prevent layouts distored

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
   before_action :set_group
-  
+
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
@@ -9,10 +9,10 @@ class MessagesController < ApplicationController
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to group_messages_path(@group), notie: 'メッセージが送信されました'
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
     else
       @messages = @group.messages.includes(:user)
-      flash.now[:alert] = 'メッセージを入力してください'
+      flash.now[:alert] = 'メッセージを入力してください。'
       render :index
     end
   end
