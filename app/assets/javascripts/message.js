51f4ff3670a2c2c3e1bf6b53bd79d1d1d297495f
nissy7ok <nishimrua.n.ku@gmail.com>
2020-07-24 21:19:06 +0900

    create html when json return 'done'
    html is devided case by if it has image.

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,40 @@
 $(function() {
+  function buildHTML(message) {
+    if (message.image) {
+      let html =
+        `<div class="Message-box">
+          <div class="Message-info">
+            <div class="Message-info__user-name">
+              ${message.user_name}
+            </div>
+            <div class="Message-info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <p class="Message-box__message">
+            ${message.message}
+          </p>
+          <img class="Message__image" src="${message.image}">
+        </div>` 
+      return html;
+    } else {
+      let html =
+      `<div class="Message-box">
+        <div class="Message-info">
+          <div class="Message-info__user-name">
+            ${message.user_name}
+          </div>
+          <div class="Message-info__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <p class="Message-box__message">
+          ${message.message}
+          </p>
+          </div>`
+      return html
+    }
+  }
   $('.Form').on('submit', function(e) {
     e.preventDefault()
     let formData = new FormData(this);
@@ -10,6 +46,9 @@ $(function() {
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
     })
   })
 });
